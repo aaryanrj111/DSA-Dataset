7e3ceb3acfb199889fee48f39586c4f98fdf198a
aaryanrj111 <80622917+aaryanrj111@users.noreply.github.com>
2025-03-17 14:50:19 +0530

    Add files via upload

---
+++ Strings/Remove consonants from a string/submissions/Correct_1/Solution.java
@@ -0,0 +1,49 @@
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String removeConsonants(String s)
+    {
+        //code here.
+    
+        StringBuilder sb = new StringBuilder();
+        
+        String s1 = "No Vowel";
+        
+        int count =0;
+        
+        for (int i=0; i<s.length() ; i++){
+            
+         char ch = s.charAt(i);
+            
+            
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+                
+                
+                   sb.append(s.charAt(i));
+                   count++;
+            }
+            
+            
+        }
+        
+        if(count==0){
+            
+            return s1;
+            
+        }
+        
+        else {
+        
+        
+        return sb.toString();
+        
+    }
+}
+
+
+
+}
